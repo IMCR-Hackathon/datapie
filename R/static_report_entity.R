f2b986ff1d4d5098a4f721240ba8b981bc87cd85
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2019-08-13 11:15:24 -0500

    update report templates
    - better column names
    - better styling of kable HTML tables
    - fix typo in static_report_entity() roxygen

--- R/static_report_entity.R
+++ R/static_report_entity.R
@@ -3,7 +3,7 @@
 #' 
 #' @description 
 #' 
-#' @param entity_df (data.frane) (data.frame) A data.frame containing entity-level data. The "data" child element of an entity-level list object in metajam output format.
+#' @param entity_df (data.frame) (data.frame) A data.frame containing entity-level data. The "data" child element of an entity-level list object in metajam output format.
 #'
 #' @return (list) A list containing entity-level report items.
 #'
